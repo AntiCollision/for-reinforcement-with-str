dc9626024324422a8d06582026fe235d2ced8937
JooHyoung Cha <chacha@udon.party>
2023-09-19 01:14:52 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 from tools import *
 import time
 import random as rd
-t = Tools(opt=Option("210.107.245.152", 7005, 3333, "-", 0))
-# t.Stop() 
-# t.Start()
 
-for _ in range(1000):
+t = Tools(opt=Option("210.107.245.190", 7000, 3333, "-", 0))
+t.Start()
+
+for _ in range(10):
     data = int(rd.random() * 2000) - 1000
-    # print(t.State())
+    print(t.State())
     t.Action(joy=[Serial(4, data)])
-    time.sleep(1)
+    time.sleep(0.5)
     
 t.Stop()
