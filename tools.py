28362fd7b09f779bca8ead37022759a11a8cd2bf
JooHyoung Cha <chacha@udon.party>
2023-09-19 12:38:00 +0900

    nmea 파서 도입함.

--- tools.py
+++ tools.py
@@ -3,12 +3,22 @@ from option import Option, Serial
 import logging as log
 import datetime as dt
 import time
+import json
+import pynmea2
 
-data = []
 
 # def callback(rawdata, addr):
 #     data.append((dt.datetime.now(), rawdata))
         
+def parse(date, text):
+    try:
+        return (date, pynmea2.parse(text.replace(" ", "")))
+    except:
+        return None
+
+def fil(obj):
+    return obj != None
+
 class Tools:
     def __init__(self, opt:Option) -> None:
         self.opt = opt
@@ -19,10 +29,13 @@ class Tools:
 
     def Stop(self):
         net.ScenarioStop(self.opt.getStrIP(), self.opt.getStrMacroPort())
-        time.sleep(10)
+        time.sleep(8)
     
     def State(self):
-        return net.NmeaReceiver(self.opt.getStrIP(), self.opt.getStrMacroPort())
+        par = json.loads(net.NmeaReceiver(self.opt.getStrIP(), self.opt.getStrMacroPort()))
+        conv = map(lambda x: parse(x['time'], x['data']), par['log'])
+        res = list(filter(fil, conv))
+        return res
 
     def Action(self, joy: [Serial]):
         for item in joy:
